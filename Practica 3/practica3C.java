9ae42a59047291fd921d12e2c3c9875dda5ab332
hewittdylan <dylanhewittmartinez@gmail.com>
2025-03-15 00:35:54 +0100

    Practica 3C con errores

--- Practica 3/practica3C.java
+++ Practica 3/practica3C.java
@@ -1,16 +1,19 @@
+import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 class Contador {
-    private final int maximo;
     private volatile int valor;
 
-    public Contador(int maximo) {
-        this.maximo = maximo;
+    public Contador() {
         this.valor = 0;
     }
 
     public void incrementar() {
-        this.valor = (this.valor + 1) % this.maximo;
+        this.valor++;
+    }
+
+    public void decrementar() {
+        this.valor--;
     }
 
     public int getValor() {
@@ -32,63 +35,122 @@ class Producto {
 
 interface Almacen {
     void escribir(Producto producto, int pos);
-    Producto leer(int pos);
+    void leer(int pos);
+    int size();
 }
 
-class AlmacenLibros implements Almacen {
-    private Producto[] productos;
+class AlmacenEL implements Almacen {
+    private final Producto[] productos;
     private final Semaphore writers;
     private final Semaphore readers;
-    private final Semaphore mutex;
+    private final Semaphore mutex;  //Para exlusión mutua
+    private final Contador activeWriters;
+    private final Contador activeReaders;
+    private final Contador waitingWriters;
+    private final Contador waitingReaders;
     private final int capacidad;
 
-    public AlmacenLibros(Semaphore writers, Semaphore readers, Semaphore mutex, int capacidad) {
+    public AlmacenEL(Semaphore writers, Semaphore readers, Semaphore mutex, int capacidad) {
         this.writers = writers;
         this.readers = readers;
         this.mutex = mutex;
-        this.capacidad = capacidad;
         this.productos = new Producto[capacidad];
+        this.capacidad = capacidad;
+        this.activeWriters = new Contador();
+        this.activeReaders = new Contador();
+        this.waitingWriters = new Contador();
+        this.waitingReaders = new Contador();
     }
 
     @Override
-    public void almacenar(Producto producto) {
+    public int size() {
+        return this.capacidad;
+    }
+
+    @Override
+    public void escribir(Producto producto, int pos) {
         try {
-            vacio.acquire();  //Si el buffer está lleno espera
-            mutexP.acquire();
-            productos[fin.getValor()] = producto;  //Guardamos los elementos por el final
-            fin.incrementar();
-            System.out.println("[PRODUCTOR] Almacenado producto: " + producto.getId());
-            mutexP.release();
-            lleno.release();  //Notifica que hay un producto disponible
+            mutex.acquire();  //Exclusión mutua
+
+            if (0 < activeWriters.getValor() || 0 < activeReaders.getValor()) {  //Si hay escritores o lectores activos
+                waitingWriters.incrementar();  //Escritor en espera
+                mutex.release();
+                writers.acquire();  //Espera su turno
+            } 
+
+            activeWriters.incrementar();  //Escritor activo
+            mutex.release();
+
+            //Escritura del producto
+            productos[pos] = producto;
+            System.out.println("[ESCRITOR] Posición: " + pos + ", contenido: " + producto.getId());
+
+            mutex.acquire();
+            activeWriters.decrementar();
+
+            //Prioridad a lectores en espera
+            if (0 < waitingReaders.getValor()) {
+                waitingReaders.decrementar();
+                readers.release();
+            }
+
+            //Si hay escritores en espera, los desbloquea
+            if (0 < waitingWriters.getValor()) {
+                waitingWriters.decrementar();
+                writers.release();
+            } else {
+                mutex.release();
+            }
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public Producto extraer() {
-        Producto prod = null;
+    public void leer(int pos) {
         try {
-            lleno.acquire();  //Espera si el buffer está vacío
-            mutexC.acquire();
-            prod = productos[ini.getValor()];
-            ini.incrementar();
-            System.out.println("[CONSUMIDOR] Consumido producto: " + (prod == null ? "null" : prod.getId()));
-            mutexC.release();
-            vacio.release();  //Notifica que el buffer está vacío
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } 
-        return prod;
+            mutex.acquire();  //Exclusión mutua
+
+            if (0 < activeReaders.getValor()) {  //Si hay escritores activos
+                waitingReaders.incrementar();  //Aumenta el número de lectores en esperaa
+                mutex.release();  //Libera exclusión mutua
+                readers.acquire();  //Espera hasta que no haya escritores
+            }
+
+            activeReaders.incrementar();  //Aumenta el número de lectores activos
+
+            if (0 < waitingReaders.getValor()) {  //Si hay lectores en espera
+                waitingReaders.decrementar();
+                readers.release();  //Libera otro lector
+            } else {
+                mutex.release();
+            }
+
+            System.out.println("[LECTOR]   Posición: " + pos + ", contenido: " + (productos[pos] == null ? "null" : productos[pos].getId()));
+
+            mutex.acquire();
+            activeReaders.decrementar();
+
+            if (activeReaders.getValor() == 0 && 0 < waitingReaders.getValor()) {  //Si es el último lector y hay escritores en espera
+                waitingReaders.decrementar();
+                writers.release();  //Libera un escritor
+            } else {
+                mutex.release();
+            }
+
+        } catch (Exception e) {
+
+        }
     }
 }
 
-class Productor extends  Thread {
+class Escritor extends  Thread {
     private final Almacen almacen;
     private final int P;
     private final int id;
 
-    public Productor(Almacen almacen, int P, int id) {
+    public Escritor(Almacen almacen, int P, int id) {
         this.almacen = almacen;
         this.P = P;
         this.id = id;
@@ -96,20 +158,21 @@ class Productor extends  Thread {
 
     @Override
     public void run() {
+        Random rand = new Random();
         for (int i = 0; i < P; i++) {
             String idProducto = Integer.toString(id) + " - " + Integer.toString(i);
             Producto prod = new Producto(idProducto);
-            almacen.almacenar(prod);
+            almacen.escribir(prod, rand.nextInt(almacen.size()));
         }
     }
 }
 
-class Consumidor extends  Thread {
+class Lector extends  Thread {
     private final Almacen almacen;
     private final int C;
     private final int id;
 
-    public Consumidor(Almacen almacen, int C, int id) {
+    public Lector(Almacen almacen, int C, int id) {
         this.almacen = almacen;
         this.C = C;
         this.id = id;
@@ -117,25 +180,26 @@ class Consumidor extends  Thread {
 
     @Override
     public void run() {
+        Random rand = new Random();
         for (int i = 0; i < C; i++) {
-            almacen.extraer();
+            almacen.leer(rand.nextInt(almacen.size()));
         }
     }
 }
 
 public class practica3C {
     public static void main(String[] args) {
-        int numEscritores = 100;
-        int numLectores = 50;
-        int P = 1;  //Num productos por escritor
-        int C = 10;  //Num productos por lector
-        int capacidadAlmacen = 10;
+        int numEscritores = 5;
+        int numLectores = 5;
+        int P = 2;  //Num productos por escritor
+        int C = 5;  //Num productos por lector
+        int capacidadAlmacen = 3;
 
         Semaphore writers = new Semaphore(0);
         Semaphore readers = new Semaphore(0);
         Semaphore mutex = new Semaphore(1);
 
-        Almacen almacen = new AlmacenProductos(lleno, vacio, mutexP, mutexC, capacidadAlmacen);
+        Almacen almacen = new AlmacenEL(writers, readers, mutex, capacidadAlmacen);
 
         Escritor[] escritores = new Escritor[numEscritores];
         Lector[] lectores = new Lector[numLectores];
